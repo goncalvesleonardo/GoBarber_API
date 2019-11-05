eafe98882bdc1b49db3f451b7348856b21cd9c20
Leonardo Gonçalves <leonardo.goncalves@totvs.com.br>
2019-11-05 15:03:04 -0300

    Commit inicial do projeto, criado api de usuarios metodos para cadastrar e alterar, e autenticacao de login com JWT

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'leonardo',
+    password: 'leonardo',
+    database: 'gobarber',
+    define: {
+        timestamps: true,
+        underscored: true,
+        undescoredAll: true,
+    },
+};
